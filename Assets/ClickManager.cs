f0adb2d15493a199d32007df1297cfba8df2a61d
Baumdiggity <atomnomicbomb@gmail.com>
2020-07-30 15:05:10 -0700

    Created Basic Text interface and added packaged for Text Mesh Pro

---
+++ Assets/ClickManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Yarn.Unity;
+
+public class ClickManager : MonoBehaviour
+{
+    DialogueUI dialogueUI;
+
+    private void Start()
+    {
+        dialogueUI = FindObjectOfType<DialogueUI>();
+    }
+
+    private void Update()
+    {
+        if(Input.GetMouseButtonDown(0) && Input.mousePosition.y < Screen.height/4)
+        {
+            dialogueUI.MarkLineComplete();
+        }
+    }
+}
