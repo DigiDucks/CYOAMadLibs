efb3edb715789ebc66e72f351f784ef0479d0d71
Baumdiggity <atomnomicbomb@gmail.com>
2020-07-31 11:07:25 -0700

    Added a way to change the background calling from Yarn

---
+++ Assets/Scripts/BackgroundManager.cs
@@ -0,0 +1,32 @@
+﻿using Google.Protobuf.WellKnownTypes;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Yarn.Unity;
+
+public class BackgroundManager : MonoBehaviour
+{
+    public Image backgroundImage;
+
+    public Sprite[] backgrounds;
+
+    [YarnCommand("changeBackground")]
+    public void ChangeBackground(string name)
+    {
+        int index = 0;
+        switch (name)
+        {
+            case "placeholder0": index = 0;
+                break;
+            case "placeholder1": index = 1;
+                break;
+            default: Debug.LogError("Incorrect Background Name called: " + name);
+                return;
+        }
+
+        backgroundImage.sprite = backgrounds[index];
+    }
+
+
+}
